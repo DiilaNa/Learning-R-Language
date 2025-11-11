465d6bbcb9bd899d1725a3e198f26670f6853d20
DiilaNa <liyanaarachchidilan@gmail.com>
2025-11-11 13:34:03 +0530

    Comparison Operators

--- Basics-R-Language/Operators.r
+++ Basics-R-Language/Operators.r
@@ -22,3 +22,26 @@ print(paste("Product:", product))
 print(paste("Division:", division))   
 print(paste("Modulus:", modules))
 print(paste("Exponentiation:", power))
+
+# Comparison Operators
+
+# Equal to
+equal_to <- (a == b)
+# Not equal to
+not_equal_to <- (a != b)
+# Greater than
+greater_than <- (a > b)
+# Less than
+less_than <- (a < b)
+# Greater than or equal to
+greater_than_equal_to <- (a >= b)
+
+# Less than or equal to
+less_than_equal_to <- (a <= b)
+
+print(paste("Equal to:", equal_to))
+print(paste("Not equal to:", not_equal_to))
+print(paste("Greater than:", greater_than))
+print(paste("Less than:", less_than))
+print(paste("Greater than or equal to:", greater_than_equal_to))
+print(paste("Less than or equal to:", less_than_equal_to)
