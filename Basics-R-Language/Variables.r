8f40d200efcd02d12085458792e10cf4b92951ed
DiilaNa <liyanaarachchidilan@gmail.com>
2025-11-11 11:48:27 +0530

    Variable Declartions

---
+++ Basics-R-Language/Variables.r
@@ -0,0 +1,17 @@
+# Variables in R
+
+# Declaration of variables
+a <- 10
+b <- 5
+c <- a + b
+# Print the results
+print(paste("Value of a:", a))
+print(paste("Value of b:", b))
+print(c)
+
+# Logical Variables and Boolean Values
+logical_var1 <- TRUE
+logical_var2 <- FALSE
+
+print(logical_var1)
+print(logical_var2)
