161184fce8b6cac7f16b4bceca3a4efcaccd5621
DiilaNa <liyanaarachchidilan@gmail.com>
2025-11-11 12:00:43 +0530

    Arithmetic operators in R

--- Basics-R-Language/Operators.r
+++ Basics-R-Language/Operators.r
@@ -1,3 +1,24 @@
 a <- 8
 b <- 3
 
+# Arithmetic Operators
+
+# Addition
+sum <- a + b
+
+# Difference
+difference <- a - b
+# Multiplication
+product <- a * b
+# Division
+division <- a / b
+# Modulus
+modules <- a %% b
+# Exponentiation
+power <- a ^ b
+print(paste("Sum:", sum))
+print(paste("Difference:", difference))
+print(paste("Product:", product))
+print(paste("Division:", division))   
+print(paste("Modulus:", modules))
+print(paste("Exponentiation:", power))
