f1ac1a831aee3a61b24abdc7afe21661b76ed876
DiilaNa <liyanaarachchidilan@gmail.com>
2025-11-11 12:13:44 +0530

    Variable Types

--- Basics-R-Language/Variables.r
+++ Basics-R-Language/Variables.r
@@ -9,9 +9,27 @@ print(paste("Value of a:", a))
 print(paste("Value of b:", b))
 print(c)
 
+# Variable Types
+
+# String Variable
+name <- "Dilan"
+
+# Numeric Variable
+age <- 22
+
+# Double/Float Variable
+height <- 5.7
+
 # Logical Variables and Boolean Values
 logical_var1 <- TRUE
 logical_var2 <- FALSE
 
 print(logical_var1)
 print(logical_var2)
+# In R can’t use + to concatenate strings like in Python or JavaScript.
+
+#  paste() adds a space betwenn arguements
+print(paste(name,age,height))
+
+# paste0() does not add any space between arguements
+print(paste0(name,age,height))
