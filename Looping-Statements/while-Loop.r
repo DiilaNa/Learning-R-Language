2b3a2906ce9bd5a57ad7bcdd5b2ea8987908bf8e
DiilaNa <liyanaarachchidilan@gmail.com>
2025-11-12 13:16:10 +0530

    add while loop

--- Looping-Statements/while-Loop.r
+++ Looping-Statements/while-Loop.r
@@ -4,3 +4,14 @@ while(count <= 5){
     print(paste("Count :",count))
     count <- count + 1
 }
+
+# calculate the sum of first 10 natural numbers
+i <- 1
+sum <- 0
+while(i <= 10){
+    sum <- sum + i 
+    i <- i + 1
+}
+
+print(paste("Sum of first 10 natural numbers is:",sum))
+
